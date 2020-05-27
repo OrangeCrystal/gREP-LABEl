882a3ef9df2512b83332f45ff154d1ed22dd96d8
OrangeCrystal <94854250+OrangeCrystal@users.noreply.github.com>
2020-05-27 08:30:10 +0000

    Minor update of trails

---
+++ Assets/Common/Enumeration/Algorithms.cs
@@ -0,0 +1,20 @@
+
+﻿namespace Common.Enumeration
+{
+	public enum Algorithms
+	{
+		MDPPolicy,
+		MDPValue,
+		Player,
+		MCESOnFirst,
+		MCESOffFirst,
+		MCESOnEvery,
+		MCESOffEvery,
+		MCOnFirst,
+		MCOffFirst,
+		MCOnEvery,
+		MCOffEvery,
+		SARSA,
+		QLEARNING
+	}
+}
