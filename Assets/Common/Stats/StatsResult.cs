39e97f91823f35a85d36c4f59d0c718606f57d0d
OrangeCrystal <94854250+OrangeCrystal@users.noreply.github.com>
2020-05-30 13:30:31 +0000

    buglet

--- Assets/Common/Stats/StatsResult.cs
+++ Assets/Common/Stats/StatsResult.cs
@@ -1,2 +1,12 @@
 ﻿using System.Collections.Generic;
-using System.I
+using System.IO;
+using Common.Core;
+using Newtonsoft.Json.Linq;
+
+namespace Common.Benchmark
+{
+	public class StatsResult<TGameState>
+	where TGameState : IGameState<TGameState>
+	{
+		public double TrainingTime { get; set; }
+		public long Evaluations { get;
