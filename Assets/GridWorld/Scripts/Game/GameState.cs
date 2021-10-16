a91340417157ca62146d41ce103f2004d61d3468
OrangeCrystal <94854250+OrangeCrystal@users.noreply.github.com>
2021-10-16 18:51:26 +0000

    [maven-release-plugin] prepare release v0.0.3

--- Assets/GridWorld/Scripts/Game/GameState.cs
+++ Assets/GridWorld/Scripts/Game/GameState.cs
@@ -3,3 +3,14 @@ using Common.Enumeration;
 using UnityEngine;
 
 namespace GridWorld.Game
+{
+    public class GameState : IGameState<GameState>
+    {
+        public TileType[,] Grid;
+        public Vector2Int AgentPos;
+
+        public GameState Copy()
+        {
+            return new GameState
+            {
+                Grid = G
