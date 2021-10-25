ca887553f05e2be76d7ceef4ed019c8589222458
OrangeCrystal <94854250+OrangeCrystal@users.noreply.github.com>
2021-10-25 09:38:44 +0000

    Overhaul

--- Assets/GridWorld/init.cs
+++ Assets/GridWorld/init.cs
@@ -4,3 +4,16 @@ using Sokoban;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
+namespace GridWorld
+{
+    public class init : MonoBehaviour
+    {
+        public GameObject GameManager;
+
+        public GameObject displayText;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Gam
