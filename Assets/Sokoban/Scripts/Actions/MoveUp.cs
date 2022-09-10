d977176e7339ce3c7a45c8dec6ad8ebb2ffb4284
OrangeCrystal <94854250+OrangeCrystal@users.noreply.github.com>
2022-09-10 21:21:31 +0000

    Added myself to AUTHORS

--- Assets/Sokoban/Scripts/Actions/MoveUp.cs
+++ Assets/Sokoban/Scripts/Actions/MoveUp.cs
@@ -6,4 +6,10 @@ namespace Sokoban.Actions
     public class MoveUp : Move
     {
         public override GameState Apply(GameState state)
- 
+        {
+            Vector2Int direction = new Vector2Int(0, 1);
+
+            return ApplyMovement(state, direction);
+        }
+    }
+}
