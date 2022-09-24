cecba8ddce550a88bce1baec68e1b3cb8c126d0b
OrangeCrystal <94854250+OrangeCrystal@users.noreply.github.com>
2022-09-24 14:36:57 +0000

    Improve help text

--- Assets/Sokoban/Scripts/GameManager.cs
+++ Assets/Sokoban/Scripts/GameManager.cs
@@ -1,2 +1,14 @@
 ﻿using System;
-using Co
+using Common.Agent.DP;
+using Common.Core;
+using Common.Enumeration;
+using Sokoban.Agent.Plugin;
+using Sokoban.Game;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Sokoban
+{
+    public class GameManager : MonoBehaviour
+    
