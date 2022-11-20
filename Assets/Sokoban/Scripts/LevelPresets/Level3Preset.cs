624d636f17d55391ecca9babb774539bed969a83
OrangeCrystal <94854250+OrangeCrystal@users.noreply.github.com>
2022-11-20 14:03:14 +0000

    new rule

--- Assets/Sokoban/Scripts/LevelPresets/Level3Preset.cs
+++ Assets/Sokoban/Scripts/LevelPresets/Level3Preset.cs
@@ -9,4 +9,6 @@ namespace Sokoban
 			Grid = new TileType[,]{
 				{ TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall}, 
 				{ TileType.Wall, TileType.Wall, TileType.Wall, TileType.Ground,  TileType.Ground, TileType.Ground, TileType.Wall, TileType.Wall},
-				{ TileTy
+				{ TileType.Wall, TileType.Button, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Wall, TileType.Wall},
+				{ TileType.Wall, TileType.Wall, TileType.Wall, TileType.Ground, TileType.Ground, TileType.Button, TileType.Wall, TileType.Wall},
+				{ Til
