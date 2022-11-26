932a88bc10b7737f05d56313cc196e27560ced95
OrangeCrystal <94854250+OrangeCrystal@users.noreply.github.com>
2022-11-26 20:11:27 +0000

    bump to 0.2.3

--- Assets/Sokoban/Scripts/LevelPresets/Level5Preset.cs
+++ Assets/Sokoban/Scripts/LevelPresets/Level5Preset.cs
@@ -8,4 +8,7 @@ namespace Sokoban
         {
             Grid = new TileType[,]{
                 { TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall},
-                { TileType.Wall, TileType.Ground, TileType.Ground, TileType.Wall, TileType.Wall}
+                { TileType.Wall, TileType.Ground, TileType.Ground, TileType.Wall, TileType.Wall},
+                { TileType.Wall, TileType.Ground, TileType.Ground, TileType.Ground , TileType.Wall},
+                { TileType.Wall, TileType.Button, TileType.Ground, TileType.Ground, TileType.Wall},
+                { T
