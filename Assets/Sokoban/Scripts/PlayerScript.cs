ccbfaa7afc46d1261812ce4c73df228447af86f9
OrangeCrystal <94854250+OrangeCrystal@users.noreply.github.com>
2022-12-04 20:05:37 +0000

    Fixed a minor bug.

--- Assets/Sokoban/Scripts/PlayerScript.cs
+++ Assets/Sokoban/Scripts/PlayerScript.cs
@@ -1,1 +1,12 @@
-﻿using Commo
+﻿using Common.Core;
+using Common.Enumeration;
+using Sokoban.Agent;
+using Sokoban.Game;
+using UnityEngine;
+
+namespace Sokoban
+{
+    public class PlayerScript : MonoBehaviour
+    {
+        private PlayerAgent _agent;
+        public GameManager GameManager { g
