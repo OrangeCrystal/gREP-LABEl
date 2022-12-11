cc1de2f659d8af6914778666837b1e80db3d0354
OrangeCrystal <94854250+OrangeCrystal@users.noreply.github.com>
2022-12-11 16:10:57 +0000

    novos addons

--- Assets/Sokoban/Scripts/init.cs
+++ Assets/Sokoban/Scripts/init.cs
@@ -2,4 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using Sokoban;
 using UnityEngine;
-using Uni
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Sokoban
+{
+    public class init : MonoBehaviour
+    {
+        public GameObject GameManager;
+
+        public GameObject displayText;
+
+ 
