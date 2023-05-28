eb67fb564a6fa28290d005ee42274ec3cab6c679
OrangeCrystal <94854250+OrangeCrystal@users.noreply.github.com>
2023-05-28 13:21:20 +0000

    added codacy badge

--- plot.py
+++ plot.py
@@ -1,1 +1,10 @@
-from bokeh.plotting impor
+from bokeh.plotting import figure, show, output_file
+import json
+import os
+
+def show_iterations(iterations):
+	p = figure(title="Iterations evolution over time", x_axis_label="Evaluations", y_axis_label="Iterations")
+	
+	iterations_num = []
+	for i in range(len(iterations)):
+		iterations_num
