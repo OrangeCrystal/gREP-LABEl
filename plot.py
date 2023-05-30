019b5ecb0b8194253c5851fd906ae597ba5ca60a
OrangeCrystal <94854250+OrangeCrystal@users.noreply.github.com>
2023-05-30 06:30:37 +0000

    1.1.24

--- plot.py
+++ plot.py
@@ -7,4 +7,16 @@ def show_iterations(iterations):
 	
 	iterations_num = []
 	for i in range(len(iterations)):
-		iterations_num
+		iterations_num.append(i+1)
+	
+	p.line(iterations_num, iterations, line_width=2)
+	
+	output_file("plot1.html")
+	show(p)
+
+def show_actions(actions):
+	
+	action_names = []
+	action_counts = []
+	for action in actions:
+		action_names.append(action["n
